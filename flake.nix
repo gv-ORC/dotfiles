bd39d71ff79e021a7199471ab431d304dde346af
Gio (Decapo) <g.viscardi@orchub.net>
2026-09-13 12:36:37 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "I be Decapo, this be my flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      # use "nixos", or your hostname as the name of the configuration
+      # it's a better practice than "default" shown in the video
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./configuration.nix
+          # inputs.home-manager.nixosModules.default
+          {
+            environment.systemPackages = with pkgs; [
+              git
+              vscodium
+              sublime-merge
+              gtkwave
+              verilator
+              python3
+              wireshark
+              gcc
+              go
+              cmake
+              spotify
+              slack
+              steam
+            ];
+          }
+          #./git.nix
+        ];
+      };
+    };
+}
