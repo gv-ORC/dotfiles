c7184f92b05fac4cc7a15ccd8403cb03e51bd17b
Gio (Decapo) <g.viscardi@orchub.net>
2026-09-15 08:28:55 -0400

    attempt to fix slang lol

--- flake.nix
+++ flake.nix
@@ -28,6 +28,7 @@
           {
             environment.systemPackages = with pkgs; [
               git
+              slang
               vscodium
               sublime-merge
               gtkwave
